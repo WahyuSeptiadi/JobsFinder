04a5806b815515f504d476fb3bc7dc1b9524a418
WahyuSeptiadi <arstuw@gmail.com>
2020-10-07 16:53:32 +0700

    update struktur code

--- app/src/main/java/com/codeart/jobsfinder/Fragment/searchFragment.kt
+++ app/src/main/java/com/codeart/jobsfinder/Fragment/searchFragment.kt
@@ -1,10 +1,11 @@
-package com.codeart.jobsfinder
+package com.codeart.jobsfinder.Fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.codeart.jobsfinder.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
